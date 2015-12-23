1c9da48274e8052317449eb2f442664d7696e2ed
shen4-4 <951630322@qq.com>
2015-12-24 01:02:18 +0800

    jquery end

--- 19-jquery.js
+++ 19-jquery.js
@@ -71,11 +71,72 @@
 	//从所有元素中移除所有jquery事件处理程序
 	$('*').unbind();
 
-	//.trigger()事件
+	//.trigger()事件 调用trigger()时会传入事件类型字符串作为第一个参数，trigger事件会在jquery对象中所有的元素上触发该类型字符串作为第一个参数
+	$("form").submit();
 
+	$("form").trigger('submit');
 
+	//在手动触发事件时，给trigger()传入第二个参数时，第二参数会成为每个触发事件处理程序的第二参数
+	$("#button").trigger('click',true);
+	$("#button").trigger('click',[x,y,z]); //传入三个额外参数
 
+	//实时事件
+	//使用bind()给元素绑定了事件处理程序，接着又创建了带有<a>元素的新文档内容，新元素和老元素不会拥有相同的事件处理程序
+	//需要使用delegate()和undelegate()方法来代替和替代bind()和unbind()
+	//调用的时候传入一个选择器字符串，一个jquery事件类型字符串，以及一个jQuery事件处理函数
 
+	$(document).delegate("a","mousevoer",linkHandler);
+	//live()方法同样也可以实时注册实时事件，实际中更普遍
+	x.live('click', function(event) {
+		/* Act on the event */
+	});
+	$(x.context).delegate('x.selector', 'eventType', function(event) {
+		selector
+	});
+	//注销实时事件处理程序，使用die()和undelegate()
+	$("a").die("mouseover"); //移除<a>元素上mouseover事件的所有实时处理程序
+	$("a").die("mouseover",linkHandler); // 只移除一个指定的实时处理程序
 
+	//jquery中的ajax
+	//load()方法是所有工具最简单的，向它传入一个URL，它会异步加载该URL中的内容，然后将内容插入到每一个选中元素中
+	setInterval(function(){$("status").load("status_report.html");},60000);
+	//加载显示部分
+	$("#temp").load("XXX.html" #selector);
+	//除了必须的参数，load()方法还接受两个可选参数，第一个可选的参数可追加到URL后面，与请求一起发送
+	//传入的是字符串，则会追加到URL后面，传入是对象则会被转化为一个用"&"分隔的名/值对后与请求一起发送
+	$("#temp").load("xxxx.html","zipcode=721125");
 
+	//jQuery.getScript()函数的第一个参数是JS代码的URL.异步加载和执行完成后，调用一次该回调函数
+	$.getScript('path/to/file', function(data, textStatus) {
+		/*optional stuff to do after getScript */ 
+	});
 
+	//jQuery.getJSON()函数与上个方法类似，它会获取文本，然后特殊处理一下，再调用指定的函数
+	$.$.getJSON('/path/to/file', {param1: 'value1'}, function(json, textStatus) {
+			/*optional stuff to do after success */
+	});
+
+	//.ajax()函数
+	$.ajax({
+		url: '/path/to/file',
+		type: 'default GET (Other values: POST)',
+		dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+		data: {param1: 'value1'},
+	})
+	.done(function() {
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+	//.ajaxSetup()传入一个选项对象来设置任意选项的默认值
+	$.ajaxSetup({
+		timeout:2000, //两秒后取消所有请求
+		cache:false //通过给URL添加时间戳来禁用浏览器缓存
+	});
+
+	//jquery选择器和选择方法
+	//太多，又灵活又多，看书吧
